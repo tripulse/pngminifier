a8641f10c13087421665e720cfb182114ebe0249
Tryamid <bsantanu381@gmail.com>
2020-03-21 21:00:26 +0530

    Configure to be uploaded on PyPI.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import subprocess
+
+setuptools.setup(
+    name= 'pngminifier',
+    version= subprocess.check_output(['git', 'describe', '--tags']).strip() \
+            .decode('utf-8') \
+            .split('-g')[0],
+    desciption= "a program that strips out ancillary data "
+                "from PNG files and reduces the file slightly",
+    author= "Shantanu Biswas",
+    author_email= "bsantanu381@gmail.com",
+    packages= ['pngminifier'],
+    project_urls= {
+        'Github': 'https://github.com/tripulse/pngminifier'
+    },
+    license= 'The Unlicense',
+    license_file= 'LICENSE.md',
+    keywords= [
+        'png', 'minifier', 'minification', 'truncator'
+        'exif tag remover', 'pngminify'
+    ],
+    python_requires= ">=3.8"
+)
